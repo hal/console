651f0806c05b01e462cb705b8ed11b66f9ff4222
Harald Pehl <harald.pehl@gmail.com>
2015-08-05 17:40:49 +0200

    Upgrade PatternFly to 2.0.0

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -11,7 +11,7 @@ var gulp = require('gulp'),
     copy = require('gulp-copy'),
     del = require('del');
 
-gulp.task('styles', function () {
+gulp.task('less', function () {
     return gulp.src('src/main/less/hal.less')
         .pipe(less())
         .pipe(autoprefixer({browsers: ['last 2 versions'], cascade: false}))
@@ -20,13 +20,7 @@ gulp.task('styles', function () {
         .pipe(gulp.dest('src/main/resources/org/jboss/hal/public/css'));
 });
 
-gulp.task('dependencies', function () {
-    // Web Components
-    gulp.src('bower_components/polymer/*.html')
-        .pipe(copy('src/main/resources/org/jboss/hal/public', {prefix: 1}));
-    gulp.src('bower_components/webcomponentsjs/*.min.js')
-        .pipe(copy('src/main/resources/org/jboss/hal/public', {prefix: 1}));
-
+gulp.task('copy', function () {
     // Patternfly
     gulp.src('bower_components/patternfly/dist/css/*.min.css')
         .pipe(copy('src/main/resources/org/jboss/hal/public', {prefix: 1}));
@@ -44,7 +38,7 @@ gulp.task('dependencies', function () {
     // Bootstrap
     gulp.src('bower_components/patternfly/components/bootstrap/dist/js/bootstrap.min.js')
         .pipe(copy('src/main/resources/org/jboss/hal/public', {prefix: 1}));
-    gulp.src('bower_components/patternfly/components/bootstrap-select/bootstrap-select.min.js')
+    gulp.src('bower_components/patternfly/components/bootstrap-select/dist/js/bootstrap-select.min.js')
         .pipe(copy('src/main/resources/org/jboss/hal/public', {prefix: 1}));
 
     // Fonts
@@ -56,17 +50,15 @@ gulp.task('dependencies', function () {
 
 // Clean
 gulp.task('clean', function (cb) {
-    del(['src/main/resources/org/jboss/hal/public/polymer/**',
-        'src/main/resources/org/jboss/hal/public/webcomponentsjs/**',
-        'src/main/resources/org/jboss/hal/public/patternfly/**',
+    del(['src/main/resources/org/jboss/hal/public/patternfly/**',
         'src/main/resources/org/jboss/hal/public/css/**'], cb)
 });
 
 // Default task
-gulp.task('default', ['styles', 'dependencies']);
+gulp.task('default', ['less', 'copy']);
 
 // Watch
 gulp.task('watch', function () {
     // Watch .less files
-    gulp.watch('src/main/less/**/*.less', ['styles']);
+    gulp.watch('src/main/less/**/*.less', ['less']);
 });
