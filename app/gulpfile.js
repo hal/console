cacd1decd797c36149d2faf672b93f96d42ab8d5
Harald Pehl <harald.pehl@gmail.com>
2015-09-22 11:21:57 +0200

    Refactor less imports

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -1,6 +1,6 @@
 /*!
  * gulp
- * $ npm install gulp-LESS gulp-autoprefixer gulp-minify-css gulp-rename gulp-copy del --save-dev
+ * $ npm install gulp-less gulp-autoprefixer gulp-minify-css gulp-rename gulp-copy del --save-dev
  */
 
 var gulp = require('gulp'),
@@ -15,43 +15,41 @@ gulp.task('less', function () {
     return gulp.src('src/main/less/hal.less')
         .pipe(less())
         .pipe(autoprefixer({browsers: ['last 2 versions'], cascade: false}))
-        .pipe(rename({suffix: '.min'}))
         .pipe(minifycss())
+        .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('src/main/resources/org/jboss/hal/public/css'));
 });
 
 gulp.task('copy', function () {
-    // Patternfly
-    gulp.src('bower_components/patternfly/dist/css/*.min.css')
-        .pipe(copy('src/main/resources/org/jboss/hal/public', {prefix: 1}));
-    gulp.src('bower_components/patternfly/dist/fonts/*')
-        .pipe(copy('src/main/resources/org/jboss/hal/public', {prefix: 1}));
-    gulp.src('bower_components/patternfly/dist/img/*')
-        .pipe(copy('src/main/resources/org/jboss/hal/public', {prefix: 1}));
-    gulp.src('bower_components/patternfly/dist/js/*.min.js')
-        .pipe(copy('src/main/resources/org/jboss/hal/public', {prefix: 1}));
-
-    // JQuery
-    gulp.src('bower_components/patternfly/components/jquery/dist/jquery.min.js')
-        .pipe(copy('src/main/resources/org/jboss/hal/public', {prefix: 1}));
-
-    // Bootstrap
-    gulp.src('bower_components/patternfly/components/bootstrap/dist/js/bootstrap.min.js')
-        .pipe(copy('src/main/resources/org/jboss/hal/public', {prefix: 1}));
-    gulp.src('bower_components/patternfly/components/bootstrap-select/dist/js/bootstrap-select.min.js')
-        .pipe(copy('src/main/resources/org/jboss/hal/public', {prefix: 1}));
-
     // Fonts
-    gulp.src('bower_components/patternfly/components/bootstrap/dist/fonts/*')
-        .pipe(copy('src/main/resources/org/jboss/hal/public', {prefix: 1}));
+    gulp.src('bower_components/patternfly/dist/fonts/*')
+        .pipe(copy('src/main/resources/org/jboss/hal/public/fonts', {prefix: 4}));
     gulp.src('bower_components/patternfly/components/font-awesome/fonts/*')
-        .pipe(copy('src/main/resources/org/jboss/hal/public', {prefix: 1}));
+        .pipe(copy('src/main/resources/org/jboss/hal/public/fonts', {prefix: 5}));
+    gulp.src('bower_components/patternfly/components/bootstrap/dist/fonts/*')
+        .pipe(copy('src/main/resources/org/jboss/hal/public/fonts', {prefix: 6}));
+
+    // Images
+    gulp.src('bower_components/patternfly/dist/img/*')
+        .pipe(copy('src/main/resources/org/jboss/hal/public/img', {prefix: 4}));
+
+    // JavaScript
+    gulp.src('bower_components/patternfly/dist/js/*.min.js')
+        .pipe(copy('src/main/resources/org/jboss/hal/public/js', {prefix: 4}));
+    gulp.src('bower_components/patternfly/components/jquery/dist/jquery.min.js')
+        .pipe(copy('src/main/resources/org/jboss/hal/public/js', {prefix: 5}));
+    gulp.src('bower_components/patternfly/components/bootstrap/dist/js/bootstrap.min.js')
+        .pipe(copy('src/main/resources/org/jboss/hal/public/js', {prefix: 6}));
+    gulp.src('bower_components/patternfly/components/bootstrap-select/dist/js/bootstrap-select.min.js')
+        .pipe(copy('src/main/resources/org/jboss/hal/public/js', {prefix: 6}));
 });
 
 // Clean
 gulp.task('clean', function (cb) {
-    del(['src/main/resources/org/jboss/hal/public/patternfly/**',
-        'src/main/resources/org/jboss/hal/public/css/**'], cb)
+    del(['src/main/resources/org/jboss/hal/public/css/**',
+        'src/main/resources/org/jboss/hal/public/fonts/**',
+        'src/main/resources/org/jboss/hal/public/img/**',
+        'src/main/resources/org/jboss/hal/public/js/**'], cb)
 });
 
 // Default task
