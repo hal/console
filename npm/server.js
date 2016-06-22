7e592a950968fe012fa7934cf034adcfcb852668
Harald Pehl <harald.pehl@gmail.com>
2016-06-22 17:01:46 +0200

    Update NPM configuration

--- npm/server.js
+++ npm/server.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var app = express();
 
-app.use('/', express.static('hal'));
+app.use('/', express.static(__dirname + '/hal'));
 app.listen(3000, function () {
     console.log('HAL.next listening on port 3000!');
 });
