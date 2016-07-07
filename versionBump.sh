75fbddaad455f5785aab241e3a496953eef12e33
Harald Pehl <harald.pehl@gmail.com>
2016-07-07 13:39:54 +0200

    Bump to 0.3.2

--- versionBump.sh
+++ versionBump.sh
@@ -7,7 +7,7 @@ PROGNAME=`basename "$0"`
 if [ "$#" -ne 1 ]; then
     echo "Illegal number of arguments. Use '$PROGNAME <version>'"
 else
-    mvn versions:set -DnewVersion=$1
+    mvn versions:set -Pdocker -DnewVersion=$1
     sed -i.versionsBackup "s/\"version\": \".*\",$/\"version\": \"$1\",/" app/bower.json
     sed -i.versionsBackup "s/\"version\": \".*\",$/\"version\": \"$1\",/" app/package.json
     sed -i.versionsBackup "s/\"version\": \".*\",$/\"version\": \"$1\",/" npm/package.json
