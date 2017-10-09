7431acba18d23311fbeb702ea7da737d488dab85
Harald Pehl <harald.pehl@gmail.com>
2017-10-09 10:27:56 +0200

    Bump to 0.9.1

--- app/Gruntfile.js
+++ app/Gruntfile.js
@@ -23,7 +23,7 @@ module.exports = function (grunt) {
     grunt.initConfig({
         theme: grunt.option('theme') || 'hal',
         config: {
-            version: '0.9.1-SNAPSHOT',
+            version: '0.9.1',
             bower: 'bower_components',
             less: 'src/main/less',
             public: 'src/main/resources/org/jboss/hal/public',
