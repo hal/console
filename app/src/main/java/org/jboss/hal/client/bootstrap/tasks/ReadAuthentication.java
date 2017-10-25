c8ec539db8ab284499ae0809b819b91fc2d1824e
Harald Pehl <harald.pehl@gmail.com>
2017-10-25 10:08:45 +0200

    Code format

--- app/src/main/java/org/jboss/hal/client/bootstrap/tasks/ReadAuthentication.java
+++ app/src/main/java/org/jboss/hal/client/bootstrap/tasks/ReadAuthentication.java
@@ -13,26 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-/*
- * JBoss, Home of Professional Open Source
- * Copyright 2011 Red Hat Inc. and/or its affiliates and other contributors
- * as indicated by the @author tags. All rights reserved.
- * See the copyright.txt in the distribution for a
- * full listing of individual contributors.
- *
- * This copyrighted material is made available to anyone wishing to use,
- * modify, copy, or redistribute it subject to the terms and conditions
- * of the GNU Lesser General Public License, v. 2.1.
- * This program is distributed in the hope that it will be useful, but WITHOUT A
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
- * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
- * You should have received a copy of the GNU Lesser General Public License,
- * v.2.1 along with this distribution; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA  02110-1301, USA.
- */
-
 package org.jboss.hal.client.bootstrap.tasks;
 
 import java.util.Set;
@@ -160,5 +140,4 @@ public class ReadAuthentication implements BootstrapTask {
                 .map(ModelNode::asString).collect(toSet());
         return new Role(property.getName(), baseRole, type, scope);
     }
-
 }
