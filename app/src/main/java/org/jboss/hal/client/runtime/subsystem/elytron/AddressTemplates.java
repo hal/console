a270f39b7d2898140783c01df08ecaccaaa74ec1
Claudio Miranda <claudio@claudius.com.br>
2018-08-09 09:15:15 -0300

    HAL-1498 Add new elytron resources: certificate-authority-account, custom-security-event-listener, mapped-role-mapper https://issues.jboss.org/browse/HAL-1498 * Add custom runtime operations to certificate-authority-account in runtime view. * Refactored key-store runtime resource to its own KeystoreElement, to later add custom runtime operations related to CA management.

--- app/src/main/java/org/jboss/hal/client/runtime/subsystem/elytron/AddressTemplates.java
+++ app/src/main/java/org/jboss/hal/client/runtime/subsystem/elytron/AddressTemplates.java
@@ -26,35 +26,32 @@ interface AddressTemplates {
     // ------------------------------------------------------ address (a-z)
 
     String CACHING_REALM_ADDRESS = ELYTRON_SUBSYSTEM_ADDRESS + "/caching-realm=*";
+    String CERTIFICATE_AUTHORITY_ACCOUNT_ADDRESS = ELYTRON_SUBSYSTEM_ADDRESS + "/certificate-authority-account=*";
+    String CREDENTIAL_STORE_ADDRESS = ELYTRON_SUBSYSTEM_ADDRESS + "/credential-store=*";
     String CUSTOM_MODIFIABLE_REALM_ADDRESS = ELYTRON_SUBSYSTEM_ADDRESS + "/custom-modifiable-realm=*";
     String FILESYSTEM_REALM_ADDRESS = ELYTRON_SUBSYSTEM_ADDRESS + "/filesystem-realm=*";
-    String LDAP_REALM_ADDRESS = ELYTRON_SUBSYSTEM_ADDRESS + "/ldap-realm=*";
-    String PROPERTIES_REALM_ADDRESS = ELYTRON_SUBSYSTEM_ADDRESS + "/properties-realm=*";
-
-    String CREDENTIAL_STORE_ADDRESS = ELYTRON_SUBSYSTEM_ADDRESS + "/credential-store=*";
     String FILTERING_KEY_STORE_ADDRESS = ELYTRON_SUBSYSTEM_ADDRESS + "/filtering-key-store=*";
+    String KEY_MANAGER_ADDRESS = ELYTRON_SUBSYSTEM_ADDRESS + "/key-manager=*";
     String KEY_STORE_ADDRESS = ELYTRON_SUBSYSTEM_ADDRESS + "/key-store=*";
     String LDAP_KEY_STORE_ADDRESS = ELYTRON_SUBSYSTEM_ADDRESS + "/ldap-key-store=*";
-
-    String KEY_MANAGER_ADDRESS = ELYTRON_SUBSYSTEM_ADDRESS + "/key-manager=*";
+    String LDAP_REALM_ADDRESS = ELYTRON_SUBSYSTEM_ADDRESS + "/ldap-realm=*";
+    String PROPERTIES_REALM_ADDRESS = ELYTRON_SUBSYSTEM_ADDRESS + "/properties-realm=*";
     String SECURITY_DOMAIN_ADDRESS = ELYTRON_SUBSYSTEM_ADDRESS + "/security-domain=*";
     String TRUST_MANAGER_ADDRESS = ELYTRON_SUBSYSTEM_ADDRESS + "/trust-manager=*";
 
     // ------------------------------------------------------ templates (a-z)
 
     AddressTemplate CACHING_REALM_TEMPLATE = AddressTemplate.of(CACHING_REALM_ADDRESS);
+    AddressTemplate CERTIFICATE_AUTHORITY_ACCOUNT_TEMPLATE = AddressTemplate.of(CERTIFICATE_AUTHORITY_ACCOUNT_ADDRESS);
     AddressTemplate CREDENTIAL_STORE_TEMPLATE = AddressTemplate.of(CREDENTIAL_STORE_ADDRESS);
     AddressTemplate CUSTOM_MODIFIABLE_REALM_TEMPLATE = AddressTemplate.of(CUSTOM_MODIFIABLE_REALM_ADDRESS);
     AddressTemplate FILESYSTEM_REALM_TEMPLATE = AddressTemplate.of(FILESYSTEM_REALM_ADDRESS);
     AddressTemplate FILTERING_KEY_STORE_TEMPLATE = AddressTemplate.of(FILTERING_KEY_STORE_ADDRESS);
-
     AddressTemplate KEY_MANAGER_TEMPLATE = AddressTemplate.of(KEY_MANAGER_ADDRESS);
     AddressTemplate KEY_STORE_TEMPLATE = AddressTemplate.of(KEY_STORE_ADDRESS);
-
     AddressTemplate LDAP_KEY_STORE_TEMPLATE = AddressTemplate.of(LDAP_KEY_STORE_ADDRESS);
     AddressTemplate LDAP_REALM_TEMPLATE = AddressTemplate.of(LDAP_REALM_ADDRESS);
     AddressTemplate PROPERTIES_REALM_TEMPLATE = AddressTemplate.of(PROPERTIES_REALM_ADDRESS);
-
     AddressTemplate SECURITY_DOMAIN_TEMPLATE = AddressTemplate.of(SECURITY_DOMAIN_ADDRESS);
     AddressTemplate TRUST_MANAGER_TEMPLATE = AddressTemplate.of(TRUST_MANAGER_ADDRESS);
     // @formatter:on
--- app/src/main/java/org/jboss/hal/client/runtime/subsystem/elytron/SSLPresenter.java
+++ app/src/main/java/org/jboss/hal/client/runtime/subsystem/elytron/SSLPresenter.java
@@ -16,6 +16,7 @@
 package org.jboss.hal.client.runtime.subsystem.elytron;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 import javax.inject.Inject;
 
@@ -23,21 +24,27 @@ import com.google.web.bindery.event.shared.EventBus;
 import com.gwtplatform.mvp.client.annotations.NameToken;
 import com.gwtplatform.mvp.client.annotations.ProxyCodeSplit;
 import com.gwtplatform.mvp.client.proxy.ProxyPlace;
+import org.jboss.hal.ballroom.LabelBuilder;
+import org.jboss.hal.ballroom.dialog.Dialog;
+import org.jboss.hal.ballroom.form.Form;
 import org.jboss.hal.core.finder.Finder;
 import org.jboss.hal.core.finder.FinderPath;
 import org.jboss.hal.core.finder.FinderPathFactory;
+import org.jboss.hal.core.mbui.form.ModelNodeForm;
 import org.jboss.hal.core.mvp.ApplicationFinderPresenter;
 import org.jboss.hal.core.mvp.HalView;
 import org.jboss.hal.core.mvp.HasPresenter;
 import org.jboss.hal.core.mvp.SupportsExpertMode;
 import org.jboss.hal.dmr.Composite;
 import org.jboss.hal.dmr.CompositeResult;
+import org.jboss.hal.dmr.ModelNode;
 import org.jboss.hal.dmr.NamedNode;
 import org.jboss.hal.dmr.Operation;
 import org.jboss.hal.dmr.ResourceAddress;
 import org.jboss.hal.dmr.dispatch.Dispatcher;
 import org.jboss.hal.meta.AddressTemplate;
 import org.jboss.hal.meta.Metadata;
+import org.jboss.hal.meta.MetadataRegistry;
 import org.jboss.hal.meta.StatementContext;
 import org.jboss.hal.meta.token.NameTokens;
 import org.jboss.hal.resources.Ids;
@@ -46,16 +53,21 @@ import org.jboss.hal.resources.Resources;
 import org.jboss.hal.spi.Message;
 import org.jboss.hal.spi.MessageEvent;
 import org.jboss.hal.spi.Requires;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import static org.jboss.gwt.elemento.core.Elements.p;
 import static org.jboss.hal.client.runtime.subsystem.elytron.AddressTemplates.*;
 import static org.jboss.hal.dmr.ModelDescriptionConstants.*;
 import static org.jboss.hal.dmr.ModelNodeHelper.asNamedNodes;
+import static org.jboss.hal.resources.Ids.FORM;
 
 public class SSLPresenter extends ApplicationFinderPresenter<SSLPresenter.MyView, SSLPresenter.MyProxy>
         implements SupportsExpertMode {
 
     private static final String SPACE = " ";
 
+    private MetadataRegistry metadataRegistry;
     private final FinderPathFactory finderPathFactory;
     private final StatementContext statementContext;
     private Resources resources;
@@ -68,11 +80,13 @@ public class SSLPresenter extends ApplicationFinderPresenter<SSLPresenter.MyView
             Resources resources,
             Finder finder,
             Dispatcher dispatcher,
+            MetadataRegistry metadataRegistry,
             FinderPathFactory finderPathFactory,
             StatementContext statementContext) {
         super(eventBus, view, proxy, finder);
         this.resources = resources;
         this.dispatcher = dispatcher;
+        this.metadataRegistry = metadataRegistry;
         this.finderPathFactory = finderPathFactory;
         this.statementContext = statementContext;
     }
@@ -98,11 +112,13 @@ public class SSLPresenter extends ApplicationFinderPresenter<SSLPresenter.MyView
     @Override
     protected void reload() {
         Composite composite = new Composite();
+        composite.add(operation(CERTIFICATE_AUTHORITY_ACCOUNT_TEMPLATE));
         composite.add(operation(KEY_MANAGER_TEMPLATE));
         composite.add(operation(SECURITY_DOMAIN_TEMPLATE));
         composite.add(operation(TRUST_MANAGER_TEMPLATE));
         dispatcher.execute(composite, (CompositeResult result) -> {
             int i = 0;
+            getView().updateCertificateAuthorityAccount(asNamedNodes(result.step(i++).get(RESULT).asPropertyList()));
             getView().updateKeyManager(asNamedNodes(result.step(i++).get(RESULT).asPropertyList()));
             getView().updateSecurityDomain(asNamedNodes(result.step(i++).get(RESULT).asPropertyList()));
             getView().updateTrustManager(asNamedNodes(result.step(i++).get(RESULT).asPropertyList()));
@@ -114,6 +130,157 @@ public class SSLPresenter extends ApplicationFinderPresenter<SSLPresenter.MyView
                 .param(INCLUDE_RUNTIME, true)
                 .param(CHILD_TYPE, template.lastName())
                 .build();
+    }
+
+    // ----------------- certificate authority account
+static Logger _log = LoggerFactory.getLogger("org.jboss");
+
+    void createAccount(String name) {
+        Metadata metadata = metadataRegistry.lookup(CERTIFICATE_AUTHORITY_ACCOUNT_TEMPLATE)
+                .forOperation(CREATE_ACCOUNT);
+        String id = Ids.build(CERTIFICATE_AUTHORITY_ACCOUNT, FORM);
+        String title = new LabelBuilder().label(CERTIFICATE_AUTHORITY_ACCOUNT);
+        Form<ModelNode> form = new ModelNodeForm.Builder<>(id, metadata)
+                .build();
+        form.setSaveCallback((form1, changedValues) -> {
+            ResourceAddress address = CERTIFICATE_AUTHORITY_ACCOUNT_TEMPLATE.resolve(statementContext, name);
+            Operation operation = new Operation.Builder(address, CREATE_ACCOUNT)
+                    .payload(form.getModel())
+                    .build();
+            dispatcher.execute(operation, result -> MessageEvent.fire(getEventBus(),
+                    Message.success(resources.messages().createAccountSuccess(name))),
+                    (operation1, failure) -> MessageEvent.fire(getEventBus(),
+                            Message.error(resources.messages().createAccountError(name, failure))),
+                    (operation1, exception) -> MessageEvent.fire(getEventBus(),
+                            Message.error(
+                                    resources.messages().createAccountError(name, exception.getMessage()))));
+        });
+        Dialog dialog = new Dialog.Builder(title)
+                .add(p().textContent(resources.messages().createAccountQuestion(name)).asElement())
+                .add(form.asElement())
+                .primary(resources.constants().create(), form::save)
+                .size(Dialog.Size.MEDIUM)
+                .closeOnEsc(true)
+                .cancel()
+                .build();
+        dialog.registerAttachable(form);
+        dialog.show();
+        form.edit(new ModelNode());
+    }
+
+    void deactivateAccount(String name) {
+        Metadata metadata = metadataRegistry.lookup(CERTIFICATE_AUTHORITY_ACCOUNT_TEMPLATE)
+                .forOperation(DEACTIVATE_ACCOUNT);
+        String id = Ids.build(CERTIFICATE_AUTHORITY_ACCOUNT, FORM);
+        String title = new LabelBuilder().label(CERTIFICATE_AUTHORITY_ACCOUNT);
+        Form<ModelNode> form = new ModelNodeForm.Builder<>(id, metadata)
+                .build();
+        form.setSaveCallback((form1, changedValues) -> {
+            ResourceAddress address = CERTIFICATE_AUTHORITY_ACCOUNT_TEMPLATE.resolve(statementContext, name);
+            Operation operation = new Operation.Builder(address, DEACTIVATE_ACCOUNT)
+                    .payload(form.getModel())
+                    .build();
+            dispatcher.execute(operation, result -> MessageEvent.fire(getEventBus(),
+                    Message.success(resources.messages().deactivateAccountSuccess(name))),
+                    (operation1, failure) -> MessageEvent.fire(getEventBus(),
+                            Message.error(resources.messages().deactivateAccountError(name, failure))),
+                    (operation1, exception) -> MessageEvent.fire(getEventBus(),
+                            Message.error(
+                                    resources.messages().deactivateAccountError(name, exception.getMessage()))));
+        });
+        Dialog dialog = new Dialog.Builder(title)
+                .add(p().textContent(resources.messages().deactivateAccountQuestion(name)).asElement())
+                .add(form.asElement())
+                .primary(resources.constants().deactivate(), form::save)
+                .size(Dialog.Size.MEDIUM)
+                .closeOnEsc(true)
+                .cancel()
+                .build();
+        dialog.registerAttachable(form);
+        dialog.show();
+        form.edit(new ModelNode());
+    }
+
+    void updateAccount(String name) {
+        Metadata metadata = metadataRegistry.lookup(CERTIFICATE_AUTHORITY_ACCOUNT_TEMPLATE)
+                .forOperation(UPDATE_ACCOUNT);
+        String id = Ids.build(CERTIFICATE_AUTHORITY_ACCOUNT, FORM);
+        String title = new LabelBuilder().label(CERTIFICATE_AUTHORITY_ACCOUNT);
+        Form<ModelNode> form = new ModelNodeForm.Builder<>(id, metadata)
+                .build();
+        form.setSaveCallback((form1, changedValues) -> {
+            ResourceAddress address = CERTIFICATE_AUTHORITY_ACCOUNT_TEMPLATE.resolve(statementContext, name);
+            Operation operation = new Operation.Builder(address, UPDATE_ACCOUNT)
+                    .payload(form.getModel())
+                    .build();
+            dispatcher.execute(operation, result -> MessageEvent.fire(getEventBus(),
+                    Message.success(resources.messages().updateAccountSuccess(name))),
+                    (operation1, failure) -> MessageEvent.fire(getEventBus(),
+                            Message.error(resources.messages().updateAccountError(name, failure))),
+                    (operation1, exception) -> MessageEvent.fire(getEventBus(),
+                            Message.error(
+                                    resources.messages().updateAccountError(name, exception.getMessage()))));
+        });
+        Dialog dialog = new Dialog.Builder(title)
+                .add(p().textContent(resources.messages().updateAccountQuestion(name)).asElement())
+                .add(form.asElement())
+                .primary(resources.constants().create(), form::save)
+                .size(Dialog.Size.MEDIUM)
+                .closeOnEsc(true)
+                .cancel()
+                .build();
+        dialog.registerAttachable(form);
+        dialog.show();
+        form.edit(new ModelNode());
+    }
+
+    void getMetadata(String name, Consumer<String> callback) {
+        ResourceAddress address = CERTIFICATE_AUTHORITY_ACCOUNT_TEMPLATE.resolve(statementContext, name);
+        Operation operation = new Operation.Builder(address, GET_METADATA)
+                .build();
+        dispatcher.execute(operation, result -> {
+                    callback.accept(result.toString());
+                    MessageEvent.fire(getEventBus(),
+                            Message.success(resources.messages().getMetadataSuccess(name)));
+                },
+                (operation1, failure) -> MessageEvent.fire(getEventBus(),
+                        Message.error(resources.messages().getMetadataError(name, failure))),
+                (operation1, exception) -> MessageEvent.fire(getEventBus(),
+                        Message.error(
+                                resources.messages().getMetadataError(name, exception.getMessage()))));
+    }
+
+    void changeAccountKey(String name) {
+        Metadata metadata = metadataRegistry.lookup(CERTIFICATE_AUTHORITY_ACCOUNT_TEMPLATE)
+                .forOperation(CHANGE_ACCOUNT_KEY);
+        String id = Ids.build(CERTIFICATE_AUTHORITY_ACCOUNT, FORM);
+        String title = new LabelBuilder().label(CERTIFICATE_AUTHORITY_ACCOUNT);
+        Form<ModelNode> form = new ModelNodeForm.Builder<>(id, metadata)
+                .build();
+        form.setSaveCallback((form1, changedValues) -> {
+            ResourceAddress address = CERTIFICATE_AUTHORITY_ACCOUNT_TEMPLATE.resolve(statementContext, name);
+            Operation operation = new Operation.Builder(address, CHANGE_ACCOUNT_KEY)
+                    .payload(form.getModel())
+                    .build();
+            dispatcher.execute(operation, result -> MessageEvent.fire(getEventBus(),
+                    Message.success(resources.messages().changeAccountKeySuccess(name))),
+                    (operation1, failure) -> MessageEvent.fire(getEventBus(),
+                            Message.error(resources.messages().changeAccountKeyError(name, failure))),
+                    (operation1, exception) -> MessageEvent.fire(getEventBus(),
+                            Message.error(
+                                    resources.messages().changeAccountKeyError(name, exception.getMessage()))));
+        });
+        Dialog dialog = new Dialog.Builder(title)
+                .add(p().textContent(resources.messages().changeAccountKeyQuestion(name)).asElement())
+                .add(form.asElement())
+                .primary(resources.constants().create(), form::save)
+                .size(Dialog.Size.MEDIUM)
+                .closeOnEsc(true)
+                .cancel()
+                .build();
+        dialog.registerAttachable(form);
+        dialog.show();
+        form.edit(new ModelNode());
     }
 
     void initKeyManager(String name) {
@@ -150,23 +317,26 @@ public class SSLPresenter extends ApplicationFinderPresenter<SSLPresenter.MyView
         Operation addOp = new Operation.Builder(address, READ_IDENTITY)
                 .param(IDENTITY, name)
                 .build();
-        dispatcher.execute(addOp, result -> {},
+        dispatcher.execute(addOp, result -> {
+
+                },
                 (operation, failure) -> MessageEvent.fire(getEventBus(),
                         Message.error(resources.messages().readAliasError(name, resource, failure))),
                 (operation, ex) -> MessageEvent.fire(getEventBus(),
                         Message.error(resources.messages().readAliasError(name, resource, ex.getMessage()))));
+            */
 
-*/
     }
 
     // @formatter:off
     @ProxyCodeSplit
     @NameToken(NameTokens.ELYTRON_RUNTIME_SSL)
-    @Requires({KEY_MANAGER_ADDRESS, SECURITY_DOMAIN_ADDRESS, TRUST_MANAGER_ADDRESS})
+    @Requires({CERTIFICATE_AUTHORITY_ACCOUNT_ADDRESS, KEY_MANAGER_ADDRESS, SECURITY_DOMAIN_ADDRESS, TRUST_MANAGER_ADDRESS})
     public interface MyProxy extends ProxyPlace<SSLPresenter> {
     }
 
     public interface MyView extends HalView, HasPresenter<SSLPresenter> {
+        void updateCertificateAuthorityAccount(List<NamedNode> items);
         void updateKeyManager(List<NamedNode> items);
         void updateSecurityDomain(List<NamedNode> items);
         void updateTrustManager(List<NamedNode> items);
