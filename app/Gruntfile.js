4b454780ec8b139cc3bac6aaf00745a6e2a18bfa
Harald Pehl <harald.pehl@gmail.com>
2020-07-13 17:55:01 +0200

    Bump to 3.0.23.Final

--- app/Gruntfile.js
+++ app/Gruntfile.js
@@ -34,7 +34,7 @@ module.exports = function (grunt) {
             node: 'node_modules',
             public: 'src/main/resources/org/jboss/hal/public',
             themeDir: '../themes/<%= theme %>/src/main/resources/org/jboss/hal/theme/<%= theme %>',
-            version: '3.0.22',
+            version: '3.0.23',
         },
 
         clean: {
