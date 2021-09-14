bc0dadd84e332b84b2ae2437c7667078d091b005
hpehl <harald.pehl@gmail.com>
2021-09-14 11:38:57 +0200

    Starting 3.4.x development

--- app/Gruntfile.js
+++ app/Gruntfile.js
@@ -34,7 +34,7 @@ module.exports = function (grunt) {
             node: 'node_modules',
             public: 'src/main/resources/org/jboss/hal/public',
             themeDir: '../themes/<%= theme %>/src/main/resources/org/jboss/hal/theme/<%= theme %>',
-            version: '3.3.8',
+            version: '3.4.0',
         },
 
         clean: {
