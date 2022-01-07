e3218ed32bc7e7d1ba2eda3f11ceb583901ec815
hpehl <harald.pehl@gmail.com>
2022-01-07 15:02:40 +0100

    Adjust scripts to build and publish HAL standalone

--- standalone/push-image.sh
+++ standalone/push-image.sh
@@ -2,21 +2,18 @@
 
 # Pushes the HAL standalone image to quay.io
 #
-# Parameters
-#   1. HAL version
-
-
-VERSION=$1
-REPO=quay.io/halconsole/hal
-
-
 # Prerequisites
-if [[ "$#" -ne 1 ]]; then
-    echo "Illegal number of parameters. Please use $0 <version>"
-    exit 1
-fi
+#   - Image has been built: ./build-image.sh
+# 
+# Parameters
+#   1. HAL version (optional)
 
+REPO=quay.io/halconsole/hal
 
 # This requires a valid configuration in ~/.docker/config.json
 docker login quay.io
-docker push $REPO:$VERSION
+if [[ "$#" -ne 1 ]]; then
+    docker push $REPO
+else
+    docker push $REPO:$1
+fi
